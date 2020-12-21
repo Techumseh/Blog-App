b4cb8bad72d7b5c453dfc4c8c3b98fbd586410fe
Zac Olds <zac.olds@gmail.com>
2020-12-21 14:05:47 -0600

    added browser router wrapper to App

---
+++ client/src/components/shared/Nav/Nav.jsx
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <div>
+      <Link to="/">
+        <h1>Blog App</h1>
+      </Link>
+    </div>
+  );
+};
+
+export default Nav;
